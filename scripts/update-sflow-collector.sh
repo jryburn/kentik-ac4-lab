8d28b605c99c43ecdfdf69e1fa099808e7111e48
mkrygeri <mkrygeri@gmail.com>
2025-11-15 20:05:37 -0500

    cred change frorr defaukt nokia

--- scripts/update-sflow-collector.sh
+++ scripts/update-sflow-collector.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Update sFlow collector IP on all devices
+# Update sFlow collector IP on all devices using gnmic
 DEVICES="spine1 spine2 leaf1 leaf2 leaf3 leaf4 leaf5 leaf6 leaf7 leaf8"
+USERNAME="admin"
+PASSWORD="NokiaSrl1!"
 
 for device in $DEVICES; do
     echo "Updating sFlow collector on $device..."
-    docker exec "clab-evpn-clos2-$device" sr_cli << EOF
-enter candidate
-/system sflow collector 1 collector-address 172.21.21.1
-commit now
-EOF
+    docker exec "clab-evpn-clos2-$device" gnmic -a 127.0.0.1:57411 \
+        -u "$USERNAME" -p "$PASSWORD" --insecure \
+        set --update-path "/system/sflow/collector[collector-id=1]/collector-address" \
+        --update-value "192.168.2.203"
 done
 
-echo "Done! All devices now sending sFlow to 172.21.21.1"
+echo "Done! All devices now sending sFlow to 192.168.2.203"
